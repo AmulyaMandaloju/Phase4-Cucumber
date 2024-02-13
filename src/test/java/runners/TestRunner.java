b86218d82c6ab4b792a52aa36c5206d03e67723b
Admin <admin@gmail.com>
2024-02-13 16:38:51 +0530

    Added login and search

---
+++ src/test/java/runners/TestRunner.java
@@ -0,0 +1,20 @@
+package runners;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src/test/resources/Features",
+glue= {"stepDefinition"},
+tags = "@tag",
+plugin= {"pretty", "html:target/cucumber-reports"}
+)
+public class TestRunner {
+
+
+
+}
